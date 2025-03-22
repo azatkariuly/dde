1b3ef3f4161941205e7bacc0e20be89bf2149bad
Azat Azamat <azat.kariuly@gmail.com>
2025-03-23 04:20:48 +0900

    test

--- CNN/dataset.py
+++ CNN/dataset.py
@@ -1,3 +1,4 @@
+import os
 import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, random_split
@@ -9,18 +10,19 @@ seed = 42
 torch.manual_seed(seed)
 
 transform = transforms.Compose([
-    transforms.Resize((480, 480)),  # Resize to match model input size
-    transforms.ToTensor(),          # Convert image to PyTorch tensor
-    transforms.Normalize(mean=[0.5], std=[0.5])  # Normalize (adjust if needed)
+    transforms.Resize((480, 480)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5], std=[0.5])
 ])
 
 dataset_path = "dataset"
-dataset = datasets.ImageFolder(root=dataset_path, transform=transform)
+train_dataset = datasets.ImageFolder(root=os.path.join(dataset_path, 'train'), transform=transform)
+val_dataset = datasets.ImageFolder(root=os.path.join(dataset_path, 'val'), transform=transform)
 
-# Split dataset into train and test sets (80% train, 20% test)
-train_size = int(0.8 * len(dataset))
-test_size = len(dataset) - train_size
-train_dataset, val_dataset = random_split(dataset, [train_size, test_size])
+# # Split dataset into train and test sets (80% train, 20% test)
+# train_size = int(0.8 * len(dataset))
+# test_size = len(dataset) - train_size
+# train_dataset, val_dataset = random_split(dataset, [train_size, test_size])
 
 # Create DataLoaders
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
